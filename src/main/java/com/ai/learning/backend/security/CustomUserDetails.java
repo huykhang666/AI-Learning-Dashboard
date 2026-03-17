92f42190ccdf5b3e511d08a167226244cdaa7b9c
Huy Khang <huykhang666@gmail.com>
2026-03-17 23:47:41 +0700

    feat(auth): Refresh Token

--- src/main/java/com/ai/learning/backend/security/CustomUserDetails.java
+++ src/main/java/com/ai/learning/backend/security/CustomUserDetails.java
@@ -1,4 +1,51 @@
 package com.ai.learning.backend.security;
+import com.ai.learning.backend.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
-public class CustomUserDetails {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails, OAuth2User {
+    private User user;
+    private Map<String,Object> attributes;
+
+    public static CustomUserDetails create(User user, Map<String, Object> attributes) {
+        return new CustomUserDetails(user,attributes);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getName() { return String.valueOf(user.getUserId()); }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
--- src/main/java/com/ai/learning/backend/security/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/ai/learning/backend/security/oauth2/OAuth2SuccessHandler.java
@@ -1,4 +1,34 @@
 package com.ai.learning.backend.security.oauth2;
 
-public class OAuth2SuccessHandler {
+import com.ai.learning.backend.security.CustomUserDetails;
+import com.ai.learning.backend.security.jwt.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtUtils jwtUtils;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+
+        //Retrieve the information of the recently logged-in user.
+        CustomUserDetails userDetails = (CustomUserDetails)  authentication.getPrincipal();
+
+        //Generate a "key" token.
+        String token = jwtUtils.generateRefreshToken(userDetails.getUsername());
+
+        String targetUrl = "http://localhost:5174/oauth2/callback?token=" + token;
+
+        clearAuthenticationAttributes(request);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
 }
