761196819148206d6d22abe3d2450122dccda186
Huy Khang <huykhang666@gmail.com>
2026-04-10 19:22:47 +0700

    feat: implement async chatbot

--- src/main/java/com/ai/learning/backend/service/impl/MessageServiceImpl.java
+++ src/main/java/com/ai/learning/backend/service/impl/MessageServiceImpl.java
@@ -6,21 +6,20 @@ import com.ai.learning.backend.entity.Message;
 import com.ai.learning.backend.enums.MessageRole;
 import com.ai.learning.backend.exception.AppException;
 import com.ai.learning.backend.exception.ErrorCode;
+import com.ai.learning.backend.mapper.MessageMapper;
 import com.ai.learning.backend.repository.MessageRepository;
 import com.ai.learning.backend.repository.SessionRepository;
 import com.ai.learning.backend.service.MessageService;
+import com.ai.learning.backend.service.logic.ChatBotAsyncTask;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 
 @Service
@@ -30,7 +29,8 @@ import java.util.Objects;
 public class MessageServiceImpl implements MessageService {
     MessageRepository messageRepository;
     SessionRepository sessionRepository;
-    WebClient webClient;
+    MessageMapper messageMapper;
+    ChatBotAsyncTask chatBotAsyncTask;
 
     @Override
     @Transactional
@@ -45,48 +45,31 @@ public class MessageServiceImpl implements MessageService {
                 .content(request.getContent())
                 .role(MessageRole.USER)
                 .build();
+        messageRepository.save(userMessage);
 
-        //Get the 5 most recent message
-        List<Map<String,String>> history = messageRepository
+        Message aiPendingMessage = Message.builder()
+                .learningSession(session)
+                .content("...")
+                .role(MessageRole.ASSISTANT)
+                .build();
+
+        //Get the 5 recent message
+        List<Map<String, String>> history = messageRepository
                 .findByLearningSession_LearningSessionIdOrderByCreatedAtAsc(
-                        request.getSessionId(), PageRequest.of(0,5))
+                        request.getSessionId(), PageRequest.of(0, 5))
                 .getContent()
                 .stream()
-                .map(m -> Map.of("role", m.getRole().name().toLowerCase(), "content",m.getContent()))
+                .map(m -> Map.of("role", m.getRole().name().toLowerCase(), "content", m.getContent()))
                 .toList();
-        //Call API
-        Map<String,Object> apiRequest = Map.of(
+
+        Map<String, Object> apiRequest = Map.of(
                 "session_id", request.getSessionId().toString(),
                 "query", request.getContent(),
                 "history", history
         );
 
-        log.info("Calling AI Service for Session: {}", request.getSessionId());
+        chatBotAsyncTask.callFastApiAndUpdateMessage(apiRequest, aiPendingMessage.getMessageId());
 
-        Map<String,String> aiRawResponse = webClient.post()
-                .uri("http://localhost:8000/ai/chat")
-                .bodyValue(apiRequest)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String,String>>() {})
-                .block();
-
-        assert aiRawResponse != null;
-        String aiContent = aiRawResponse.get("answer");
-
-        //Save message AI to the database
-        Message aiMessage = Message.builder()
-                .learningSession(session)
-                .content(aiContent)
-                .role(MessageRole.ASSISTANT)
-                .build();
-        messageRepository.save(aiMessage);
-
-        //Return DTO
-        return MessageResponse.builder()
-                .messageId(aiMessage.getMessageId())
-                .content(aiContent)
-                .role(aiMessage.getRole())
-                .createdAt(aiMessage.getCreatedAt())
-                .build();
+        return messageMapper.toMessageResponse(aiPendingMessage);
     }
 }
--- src/main/java/com/ai/learning/backend/service/logic/ChatBotAsyncTask.java
+++ src/main/java/com/ai/learning/backend/service/logic/ChatBotAsyncTask.java
@@ -1,4 +1,50 @@
 package com.ai.learning.backend.service.logic;
 
+import com.ai.learning.backend.repository.MessageRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class ChatBotAsyncTask {
+    WebClient webClient;
+    MessageRepository messageRepository;
+
+    @Async("taskExecutor")
+    public void callFastApiAndUpdateMessage(Map<String,Object> apiRequest, Long aiMessageId) {
+        try {
+            Map<String,String> response = webClient.post()
+                    .uri("http://localhost:8000/ai/chat")
+                    .bodyValue(apiRequest)
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<Map<String,String>>() {})
+                    .block();
+
+            if(response != null && response.containsKey("answer")) {
+                String aiAnswer = response.get("answer");
+
+                //Update Message to the database
+                messageRepository.findById(aiMessageId).ifPresent(msg -> {
+                    msg.setContent(aiAnswer);
+                    messageRepository.save(msg);
+
+                });
+            }
+        } catch (Exception e) {
+            messageRepository.findById(aiMessageId).ifPresent(msg -> {
+                msg.setContent("I can't connect to AI service right now.");
+                messageRepository.save(msg);
+            });
+        }
+    }
 }
